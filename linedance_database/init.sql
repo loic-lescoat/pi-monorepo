391737403894d5484415b7d6d6b723dfe8eeea89
Your Name <you@example.com>
2025-12-20 22:33:04 -0800

    just youtube

--- linedance_database/init.sql
+++
@@ -1,4 +0,0 @@
-
-create table dances (id int generated always as identity primary key, name varchar, keywords varchar, url varchar);
-create table progress (username varchar, id int, status int);
-create table interest (username varchar, id int, interest int);
