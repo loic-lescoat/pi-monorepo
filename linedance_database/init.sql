f015bdf5ca3ba4f051b9950b8f1e94e13448feb3
Loïc LESCOAT <loic@example.com>
2026-01-08 23:35:01 +0100

    changed db

--- linedance_database/init.sql
+++ linedance_database/init.sql
@@ -1,4 +1,4 @@
-
 create table dances (id int generated always as identity primary key, name varchar, keywords varchar, url varchar);
-create table progress (username varchar, id int, status int);
-create table interest (username varchar, id int, interest int);
+create table progress (username varchar, id int, status int, foreign key (id) references dances(id) on delete cascade); -- NOTE: should have primary key
+create table interest (username varchar, id int, interest int, foreign key (id) references dances(id) on delete cascade); -- NOTE: should have primary key
+create table dance_descriptions (id int primary key, description varchar, foreign key (id) references dances(id) on delete cascade);
